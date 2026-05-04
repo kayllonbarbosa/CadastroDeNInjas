854a520e3d6b591eee484d327d4f9fd6f64c6219
kayllonbarbosa <156196069+kayllonbarbosa@users.noreply.github.com>
2026-05-04 11:50:55 -0300

    criado a cntroller[aula2]

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/controller.java
@@ -0,0 +1,27 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class controller {
+
+    /*
+    GetMapping - puxar informaçoes
+    PostMapping - mandar informações
+    PutMapping - alterar informações
+    PatchMapping - altera informações
+    Deletemapping - deleta informações
+     */
+
+    @GetMapping("/boasvindas")
+    public String boasvindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+
+
+
+}
