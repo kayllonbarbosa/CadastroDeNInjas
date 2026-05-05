5c7754b602631967b5502f246ec1f364882185a8
kayllonbarbosa <156196069+kayllonbarbosa@users.noreply.github.com>
2026-05-05 11:25:17 -0300

    cria uma entidade no banco de dados a partir do NinjaModel[aula3]

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class controller {
+public class NinjaController {
 
     /*
     GetMapping - puxar informaçoes
